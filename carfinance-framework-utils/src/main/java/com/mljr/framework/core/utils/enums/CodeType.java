d9153d3655adb3eca7986a5a6c91e4aaec9050fe
高祥 <xiang.gao@mljr.com>
2016-05-17 10:17:52 +0800

    代码调整

--- carfinance-framework-utils/src/main/java/com/mljr/framework/core/utils/enums/CodeType.java
+++ carfinance-framework-utils/src/main/java/com/mljr/framework/core/utils/enums/CodeType.java
@@ -1,7 +1,5 @@
 package com.mljr.framework.core.utils.enums;
 
-import com.mljr.framework.core.utils.JavaEnumUtils;
-
 /**
  * @Description: 加密类型
  * @ClassName: CodeType
@@ -20,21 +18,6 @@ public enum CodeType {
 	private CodeType(String code, String description) {
 		this.code = code;
 		this.description = description;
-		JavaEnumUtils.put(this.getClass().getName() + code, this);
-	}
-	
-	/**
-	 * 一个便利的方法，方便使用者通过code获得枚举对象，
-	 * 对于非法状态，以个人处理&lt;/b&gt;
-	 * @param code
-	 * @return
-	 */
-	public static CodeType valueByCode(String code) {
-		Object obj = JavaEnumUtils.get(CodeType.class.getName() + code);
-		if (null != obj) {
-			return (CodeType) obj;
-		}
-		return AES;
 	}
 	
 	/** 编码 */
